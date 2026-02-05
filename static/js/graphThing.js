5e813b11cadfa943b1e74056a43f7408e6363530
Yeetdatpizza <118491355+Yeetdatpizza@users.noreply.github.com>
2026-02-05 15:01:45 +0000

    Added in the graph, looks good now.

--- static/js/graphThing.js
+++ static/js/graphThing.js
@@ -17,19 +17,48 @@ document.addEventListener('DOMContentLoaded', () => {
         ctx.setTransform(scale, 0, 0, scale, 0, 0);
     }
 
+    function drawGrid() {
+        const width = canvas.width;
+        const height = canvas.height;
+        const ratio = 2/3;
+        const baseSize = 30
+        const squareSizeX = width / baseSize * ratio;
+        const squareSizeY = height / baseSize;
+        
+        
+        
+        ctx.strokeStyle = '#000000';
+        ctx.lineWidth = 1;
+
+        for (let x = 0; x <= width; x += squareSizeX) {
+            ctx.beginPath();
+            ctx.moveTo(x, 0);
+            ctx.lineTo(x, height);
+            ctx.stroke();
+        }
+
+        for (let y = 0; y <= height; y += squareSizeY) {
+            ctx.beginPath();
+            ctx.moveTo(0, y);
+            ctx.lineTo(width, y);
+            ctx.stroke();
+        }
+
+        ctx.beginPath();
+        ctx.arc((width / 2) - (squareSizeX * 4.5), (height / 2) - (squareSizeY * 3), squareSizeX / 2, 0, 360);
+        ctx.stroke(); 
+
+    }
+
     resizeCanvasToParent();
     window.addEventListener('resize', resizeCanvasToParent);
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    ctx.font = '48px Comic Sans MS';
-    ctx.textAlign = 'center';
-    ctx.textBaseline = 'middle';
+    drawGrid();
 
-    const x = canvas.width  / (2 * window.devicePixelRatio);
-    const y = canvas.height / (2 * window.devicePixelRatio);
-
-    ctx.fillText('This is where the graph will go.', x, y);
-
+    window.addEventListener('resize', () => {
+        drawGrid();
+    });
 
 });
